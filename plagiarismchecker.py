a575b4f5f810f55d504a8cd4cdd8f5ea59c52cba
Ashruu15 <ashruthaashru15@gmail.com>
2023-09-30 19:27:33 +0530

    weather

--- plagiarismchecker.py
+++
@@ -1,8 +0,0 @@
-from difflib import SequenceMatcher
-
-with open("content.txt") as file1,open("sample.txt") as file2: 
-    file1data=file1.read()
-    file2data=file2.read()
-    PlagiarizedPercent=SequenceMatcher(None,file1data,file2data).ratio()
-    print("The Percentage of Plagiarism Done is : ")
-    print(PlagiarizedPercent*100)
